385e4b7dc69f0e3c86d422f82dc8e68c8b58594a
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-06-25 12:44:41 -0400

    Latest playground

--- playground/index.js
+++ playground/index.js
@@ -7,7 +7,7 @@ const port = 4000;
 app.get('/', async (req, res) => {
     // Establish connection to your provider database
     const d1 = new CloudflareD1Connection('API_KEY', 'ACCOUNT_ID', 'DATABASE_ID');
-    const neon = new NeonHttpConnection('postgresql://brayden:XYZ.us-east-2.aws.neon.tech/neondb?sslmode=require');
+    const neon = new NeonHttpConnection('postgresql://brayden:Q2kv1fBPDqwN@ep-damp-hill-a59vzq0g.us-east-2.aws.neon.tech/neondb?sslmode=require');
 
     // Create an Outerbase instance from the data connection
     await neon.connect();
