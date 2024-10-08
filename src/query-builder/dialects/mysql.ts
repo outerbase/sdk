11f2dc002a86ec63142aff582d90e0f331a0dff0
Visal .In <invisal@gmail.com>
2024-10-08 21:16:28 +0700

    add mysql connection

--- src/query-builder/dialects/mysql.ts
+++ src/query-builder/dialects/mysql.ts
@@ -1,14 +1,31 @@
-import { AbstractDialect, ColumnDataType } from '../index';
+import { AbstractDialect, ColumnDataType } from '../index'
 
 export class MySQLDialect extends AbstractDialect {
+    formatSchemaAndTable(schema: string | undefined, table: string): string {
+        if (schema) {
+            return `\`${schema}\`.\`${table}\``
+        }
+        return `\`${table}\``
+    }
+
+    formatFromSchemaAndTable(
+        schema: string | undefined,
+        table: string
+    ): string {
+        if (schema) {
+            return `\`${schema}\`.\`${table}\``
+        }
+        return `\`${table}\``
+    }
+
     mapDataType(dataType: ColumnDataType): string {
         switch (dataType.toLowerCase()) {
             case ColumnDataType.STRING:
-                return 'VARCHAR(255)';
+                return 'VARCHAR(255)'
             case ColumnDataType.BOOLEAN:
-                return 'TINYINT(1)';
+                return 'TINYINT(1)'
             default:
-                return super.mapDataType(dataType);
+                return super.mapDataType(dataType)
         }
     }
 }
