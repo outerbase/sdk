8bfff91d0cd911c50a4e8f1fd15138907e0130ad
Visal .In <invisal@gmail.com>
2024-10-13 11:40:26 +0700

    add postgre support for fetch schema

--- src/connections/mysql.ts
+++ src/connections/mysql.ts
@@ -5,6 +5,178 @@ import { QueryType } from '../query-params';
 import { Constraint, Database, Table, TableColumn } from './../models/database';
 import { MySQLDialect } from './../query-builder/dialects/mysql';
 
+interface MySQLSchemaResult {
+    SCHEMA_NAME: string;
+}
+interface MySQLTableResult {
+    TABLE_SCHEMA: string;
+    TABLE_NAME: string;
+}
+
+interface MySQLColumnResult {
+    TABLE_SCHEMA: string;
+    TABLE_NAME: string;
+    COLUMN_NAME: string;
+    COLUMN_TYPE: string;
+    ORDINAL_POSITION: number;
+    IS_NULLABLE: string;
+    COLUMN_DEFAULT: string | null;
+    COLUMN_KEY: string;
+    EXTRA: string;
+}
+
+interface MySQLConstraintResult {
+    TABLE_SCHEMA: string;
+    TABLE_NAME: string;
+    CONSTRAINT_NAME: string;
+    CONSTRAINT_TYPE: string;
+}
+
+interface MySQLConstraintColumnResult {
+    TABLE_SCHEMA: string;
+    TABLE_NAME: string;
+    COLUMN_NAME: string;
+    CONSTRAINT_NAME: string;
+    REFERENCED_TABLE_SCHEMA: string;
+    REFERENCED_TABLE_NAME: string;
+    REFERENCED_COLUMN_NAME: string;
+}
+
+// We expose this function for other drivers with similar structure
+// For example: PostgreSQL has almost the same structure with MySQL
+export function buildMySQLDatabaseSchmea({
+    schemaList,
+    tableList,
+    columnList,
+    constraintsList,
+    constraintColumnsList,
+}: {
+    schemaList: MySQLSchemaResult[];
+    tableList: MySQLTableResult[];
+    columnList: MySQLColumnResult[];
+    constraintsList: MySQLConstraintResult[];
+    constraintColumnsList: MySQLConstraintColumnResult[];
+}): Database {
+    // Create dictionary of schema
+    const result = schemaList.reduce<Database>((db, schema) => {
+        db[schema.SCHEMA_NAME] = {};
+        return db;
+    }, {});
+
+    // Table lookup by schema and table name
+    const tableLookup: Record<string, Table> = {};
+
+    // Group tables by schema and also build table lookup
+    for (const table of tableList) {
+        if (!result[table.TABLE_SCHEMA]) {
+            result[table.TABLE_SCHEMA] = {};
+        }
+
+        if (!result[table.TABLE_SCHEMA][table.TABLE_NAME]) {
+            const tableObject = {
+                name: table.TABLE_NAME,
+                columns: [],
+                indexes: [],
+                constraints: [],
+            };
+
+            tableLookup[table.TABLE_SCHEMA + '.' + table.TABLE_NAME] =
+                tableObject;
+            result[table.TABLE_SCHEMA][table.TABLE_NAME] = tableObject;
+        }
+    }
+
+    // Column lookup by schema, table and column name
+    const columnLookup: Record<string, TableColumn> = {};
+
+    // Group the columns by table and also build column lookup
+    for (const column of columnList) {
+        const table =
+            tableLookup[column.TABLE_SCHEMA + '.' + column.TABLE_NAME];
+
+        if (!table) continue;
+
+        const columnObject = {
+            name: column.COLUMN_NAME,
+            type: column.COLUMN_TYPE,
+            position: column.ORDINAL_POSITION,
+            nullable: column.IS_NULLABLE === 'YES',
+            default: column.COLUMN_DEFAULT,
+            primary: column.COLUMN_KEY === 'PRI',
+            unique: column.EXTRA === 'UNI',
+            references: [],
+        } as TableColumn;
+
+        columnLookup[
+            column.TABLE_SCHEMA +
+                '.' +
+                column.TABLE_NAME +
+                '.' +
+                column.COLUMN_NAME
+        ] = columnObject;
+
+        table.columns.push(columnObject);
+    }
+
+    // Constraint lookup by schema and constraint name
+    const constraintLookup: Record<string, Constraint> = {};
+
+    // Group constraints by table and also build constraint lookup
+    for (const constraint of constraintsList) {
+        const table =
+            tableLookup[constraint.TABLE_SCHEMA + '.' + constraint.TABLE_NAME];
+
+        if (!table) continue;
+
+        const constraintObject = {
+            name: constraint.CONSTRAINT_NAME,
+            schema: constraint.TABLE_SCHEMA,
+            tableName: constraint.TABLE_NAME,
+            type: constraint.CONSTRAINT_TYPE,
+            columns: [],
+        };
+
+        constraintLookup[
+            constraint.TABLE_SCHEMA + '.' + constraint.CONSTRAINT_NAME
+        ] = constraintObject;
+
+        table.constraints.push(constraintObject);
+    }
+
+    // Group constraint columns by constraint
+    for (const constraintColumn of constraintColumnsList) {
+        const constraint =
+            constraintLookup[
+                constraintColumn.TABLE_SCHEMA +
+                    '.' +
+                    constraintColumn.CONSTRAINT_NAME
+            ];
+
+        if (!constraint) continue;
+
+        const currentColumn =
+            columnLookup[
+                constraintColumn.TABLE_SCHEMA +
+                    '.' +
+                    constraintColumn.TABLE_NAME +
+                    '.' +
+                    constraintColumn.COLUMN_NAME
+            ];
+        if (currentColumn && constraintColumn.REFERENCED_COLUMN_NAME) {
+            currentColumn.references.push({
+                table: constraintColumn.REFERENCED_TABLE_NAME,
+                column: constraintColumn.REFERENCED_COLUMN_NAME,
+            });
+        }
+
+        constraint.columns.push({
+            columnName: constraintColumn.COLUMN_NAME,
+        });
+    }
+
+    return result;
+}
+
 export class MySQLConnection implements BaseConnection {
     protected conn: Connection;
     public dialect = new MySQLDialect();
@@ -35,165 +207,35 @@ export class MySQLConnection implements BaseConnection {
 
     async fetchDatabaseSchema(): Promise<Database> {
         // Get all the schema list
-        const { data: schemaList } = await this.query<{ SCHEMA_NAME: string }>({
+        const { data: schemaList } = await this.query<MySQLSchemaResult>({
             query: "SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')",
         });
 
-        const result = schemaList.reduce<Database>((db, schema) => {
-            db[schema.SCHEMA_NAME] = {};
-            return db;
-        }, {});
-
-        // Get table list
-        const tableLookup: Record<string, Table> = {};
-        const { data: tableList } = await this.query<{
-            TABLE_SCHEMA: string;
-            TABLE_NAME: string;
-        }>({
+        const { data: tableList } = await this.query<MySQLTableResult>({
             query: "SELECT TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE FROM information_schema.tables WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')",
         });
 
-        for (const table of tableList) {
-            if (!result[table.TABLE_SCHEMA]) {
-                result[table.TABLE_SCHEMA] = {};
-            }
-
-            if (!result[table.TABLE_SCHEMA][table.TABLE_NAME]) {
-                const tableObject = {
-                    name: table.TABLE_NAME,
-                    columns: [],
-                    indexes: [],
-                    constraints: [],
-                };
-
-                tableLookup[table.TABLE_SCHEMA + '.' + table.TABLE_NAME] =
-                    tableObject;
-                result[table.TABLE_SCHEMA][table.TABLE_NAME] = tableObject;
-            }
-        }
-
-        // Get column list
-        const columnLookup: Record<string, TableColumn> = {};
-        const { data: columnList } = await this.query<{
-            TABLE_SCHEMA: string;
-            TABLE_NAME: string;
-            COLUMN_NAME: string;
-            COLUMN_TYPE: string;
-            ORDINAL_POSITION: number;
-            IS_NULLABLE: string;
-            COLUMN_DEFAULT: string | null;
-            COLUMN_KEY: string;
-            EXTRA: string;
-        }>({
+        const { data: columnList } = await this.query<MySQLColumnResult>({
             query: "SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, ORDINAL_POSITION, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA FROM information_schema.columns WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')",
         });
 
-        for (const column of columnList) {
-            const table =
-                tableLookup[column.TABLE_SCHEMA + '.' + column.TABLE_NAME];
-
-            if (!table) continue;
-
-            const columnObject = {
-                name: column.COLUMN_NAME,
-                type: column.COLUMN_TYPE,
-                position: column.ORDINAL_POSITION,
-                nullable: column.IS_NULLABLE === 'YES',
-                default: column.COLUMN_DEFAULT,
-                primary: column.COLUMN_KEY === 'PRI',
-                unique: column.EXTRA === 'UNI',
-                references: [],
-            };
-
-            columnLookup[
-                column.TABLE_SCHEMA +
-                    '.' +
-                    column.TABLE_NAME +
-                    '.' +
-                    column.COLUMN_NAME
-            ] = columnObject;
-
-            table.columns.push(columnObject);
-        }
-
-        // Get constraints list
-        const constraintLookup: Record<string, Constraint> = {};
-        const { data: constraintsList } = await this.query<{
-            TABLE_SCHEMA: string;
-            TABLE_NAME: string;
-            CONSTRAINT_NAME: string;
-            CONSTRAINT_TYPE: string;
-        }>({
-            query: `SELECT TABLE_SCHEMA, TABLE_NAME, CONSTRAINT_NAME, CONSTRAINT_TYPE FROM information_schema.table_constraints WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys') AND CONSTRAINT_TYPE IN ('PRIMARY KEY', 'UNIQUE', 'FOREIGN KEY')`,
-        });
-
-        for (const constraint of constraintsList) {
-            const table =
-                tableLookup[
-                    constraint.TABLE_SCHEMA + '.' + constraint.TABLE_NAME
-                ];
-
-            if (!table) continue;
-
-            const constraintObject = {
-                name: constraint.CONSTRAINT_NAME,
-                schema: constraint.TABLE_SCHEMA,
-                tableName: constraint.TABLE_NAME,
-                type: constraint.CONSTRAINT_TYPE,
-                columns: [],
-            };
-
-            constraintLookup[
-                constraint.TABLE_SCHEMA + '.' + constraint.CONSTRAINT_NAME
-            ] = constraintObject;
-
-            table.constraints.push(constraintObject);
-        }
-
-        // Get constraint columns
-        const { data: constraintColumnsList } = await this.query<{
-            TABLE_SCHEMA: string;
-            TABLE_NAME: string;
-            COLUMN_NAME: string;
-            CONSTRAINT_NAME: string;
-            REFERENCED_TABLE_SCHEMA: string;
-            REFERENCED_TABLE_NAME: string;
-            REFERENCED_COLUMN_NAME: string;
-        }>({
-            query: `SELECT CONSTRAINT_NAME, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM information_schema.key_column_usage WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')`,
-        });
-
-        for (const constraintColumn of constraintColumnsList) {
-            const constraint =
-                constraintLookup[
-                    constraintColumn.TABLE_SCHEMA +
-                        '.' +
-                        constraintColumn.CONSTRAINT_NAME
-                ];
-
-            if (!constraint) continue;
-
-            const currentColumn =
-                columnLookup[
-                    constraintColumn.TABLE_SCHEMA +
-                        '.' +
-                        constraintColumn.TABLE_NAME +
-                        '.' +
-                        constraintColumn.COLUMN_NAME
-                ];
-            if (currentColumn && constraintColumn.REFERENCED_COLUMN_NAME) {
-                currentColumn.references.push({
-                    table: constraintColumn.REFERENCED_TABLE_NAME,
-                    column: constraintColumn.REFERENCED_COLUMN_NAME,
-                });
-            }
-
-            constraint.columns.push({
-                columnName: constraintColumn.COLUMN_NAME,
+        const { data: constraintsList } =
+            await this.query<MySQLConstraintResult>({
+                query: `SELECT TABLE_SCHEMA, TABLE_NAME, CONSTRAINT_NAME, CONSTRAINT_TYPE FROM information_schema.table_constraints WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys') AND CONSTRAINT_TYPE IN ('PRIMARY KEY', 'UNIQUE', 'FOREIGN KEY')`,
             });
-        }
 
-        return result;
+        const { data: constraintColumnsList } =
+            await this.query<MySQLConstraintColumnResult>({
+                query: `SELECT CONSTRAINT_NAME, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, REFERENCED_TABLE_SCHEMA, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM information_schema.key_column_usage WHERE TABLE_SCHEMA NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys')`,
+            });
+
+        return buildMySQLDatabaseSchmea({
+            schemaList,
+            tableList,
+            columnList,
+            constraintsList,
+            constraintColumnsList,
+        });
     }
 
     async connect(): Promise<any> {}
--- src/connections/postgresql.ts
+++ src/connections/postgresql.ts
@@ -4,6 +4,8 @@ import { Query } from '../query';
 import { AbstractDialect } from 'src/query-builder';
 import { PostgresDialect } from 'src/query-builder/dialects/postgres';
 import { QueryType } from 'src/query-params';
+import { Database } from 'src/models/database';
+import { buildMySQLDatabaseSchmea } from './mysql';
 
 function replacePlaceholders(query: string): string {
     let index = 1;
@@ -42,5 +44,119 @@ export class PostgreSQLConnection implements Connection {
             error: null,
             query: query.query,
         };
+    }
+
+    async fetchDatabaseSchema(): Promise<Database> {
+        // Get the list of schema first
+        const { data: schemaList } = await this.query<{ schema_name: string }>({
+            query: `SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog', 'pg_toast');`,
+        });
+
+        // Get the list of all tables
+        const { data: tableList } = await this.query<{
+            table_name: string;
+            table_schema: string;
+        }>({
+            query: `SELECT table_name, table_schema FROM information_schema.tables WHERE table_schema NOT IN ('information_schema', 'pg_catalog', 'pg_toast');`,
+        });
+
+        // Get the list of all columns
+        const { data: columnList } = await this.query<{
+            table_schema: string;
+            table_name: string;
+            column_name: string;
+            data_type: string;
+            is_nullable: string;
+            column_default: string;
+            ordinal_position: number;
+        }>({
+            query: `SELECT * FROM information_schema.columns WHERE table_schema NOT IN ('information_schema', 'pg_catalog', 'pg_toast');`,
+        });
+
+        // Get the list of all constraints
+        const { data: constraintList } = await this.query<{
+            constraint_schema: string;
+            constraint_name: string;
+            table_catalog: string;
+            table_schema: string;
+            constraint_type: string;
+        }>({
+            query: `SELECT * FROM information_schema.table_constraints WHERE constraint_schema NOT IN ('information_schema', 'pg_catalog', 'pg_toast');`,
+        });
+
+        // Get the list of foreign key relation
+        const { data: constraintColumnsList } = await this.query<{
+            constraint_name: string;
+            table_schema: string;
+            table_name: string;
+            column_name: string;
+            reference_table_name: string;
+            reference_column_name: string;
+            reference_table_schema: string;
+        }>({
+            query: `SELECT
+	kcu.constraint_name,
+	kcu.table_schema,
+	kcu.table_name,
+	kcu.column_name,
+	ccu.table_schema AS reference_table_schema,
+	ccu.table_name AS reference_table_name,
+	ccu.column_name AS reference_column_name
+FROM
+	information_schema.table_constraints AS tc
+	LEFT JOIN information_schema.key_column_usage AS kcu
+	ON (
+		tc.table_schema = kcu.table_schema AND
+		tc.table_name = kcu.table_name AND
+		tc.constraint_name = kcu.constraint_name
+	)
+	LEFT JOIN information_schema.constraint_column_usage AS ccu
+	ON (
+		ccu.table_schema = kcu.table_schema AND
+		ccu.table_name = kcu.table_name AND
+		ccu.constraint_name = kcu.constraint_name AND
+		tc.constraint_name = 'FOREIGN KEY'
+	)
+WHERE
+	kcu.constraint_schema NOT IN ('information_schema', 'pg_catalog', 'pg_toast')`,
+        });
+
+        // Postgres structure is similar to MySQL, so we can reuse the MySQL schema builder
+        // by just mapping the column names
+        return buildMySQLDatabaseSchmea({
+            schemaList: schemaList.map((schema) => ({
+                SCHEMA_NAME: schema.schema_name,
+            })),
+            tableList: tableList.map((table) => ({
+                TABLE_NAME: table.table_name,
+                TABLE_SCHEMA: table.table_schema,
+            })),
+            columnList: columnList.map((column) => ({
+                TABLE_NAME: column.table_name,
+                COLUMN_NAME: column.column_name,
+                COLUMN_TYPE: column.data_type,
+                IS_NULLABLE: column.is_nullable,
+                COLUMN_DEFAULT: column.column_default,
+                COLUMN_KEY: '',
+                EXTRA: '',
+                ORDINAL_POSITION: column.ordinal_position,
+                TABLE_SCHEMA: column.table_schema,
+            })),
+            constraintsList: constraintList.map((constraint) => ({
+                CONSTRAINT_NAME: constraint.constraint_name,
+                CONSTRAINT_TYPE: constraint.constraint_type,
+                TABLE_NAME: constraint.table_catalog,
+                TABLE_SCHEMA: constraint.table_schema,
+            })),
+            constraintColumnsList: constraintColumnsList.map((constraint) => ({
+                TABLE_NAME: constraint.table_name,
+                TABLE_SCHEMA: constraint.table_schema,
+                COLUMN_NAME: constraint.column_name,
+                CONSTRAINT_NAME: constraint.constraint_name,
+                REFERENCED_TABLE_NAME: constraint.reference_table_name,
+                REFERENCED_COLUMN_NAME: constraint.reference_column_name,
+                REFERENCED_TABLE_SCHEMA: constraint.reference_table_schema,
+            })),
+        });
     }
 }
